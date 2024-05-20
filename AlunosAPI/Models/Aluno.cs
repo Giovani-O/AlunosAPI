2ec992943061a6b6dffaa31cbf5a7676c70fd806
Giovani de Oliveira <giovani98@proton.me>
2024-05-20 10:33:36 -0300

    feat: Create Alunos entity

---
+++ AlunosAPI/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlunosAPI.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(80)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public int Idade { get; set; }
+    }
+}
